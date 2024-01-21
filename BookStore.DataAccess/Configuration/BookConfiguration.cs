fa40ed12027eb78213e2f562a15b8a337d8c0e86
chikiryava <alex.man20151@gmail.com>
2024-01-22 03:02:44 +0500

    first commit

---
+++ BookStore.DataAccess/Configuration/BookConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Configuration
+{
+    internal class BookConfiguration : IEntityTypeConfiguration<BookEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(b => b.Title)
+                .IsRequired()
+                .HasMaxLength(Book.MAX_TITLE_LENGTH);
+            builder.Property(b => b.Description)
+                .IsRequired();
+            builder.Property(b => b.Price)
+                .IsRequired();
+               
+        }
+    }
+}
